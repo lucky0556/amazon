fd8a9664157175e8c07aa5260afb19286bc3252e
lucky0556 <jhenkar695@gmail.com>
2025-04-10 12:27:51 +0530

    Update node3.js

--- node3.js
+++ node3.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const uri = "mongodb://localhost:27017"; // Replace with your MongoDB connection string
+const uri = "mongodb+srv://jhenkar695:<djtOllDTmxZGYgKy>@cluster0.vcolgra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const PORT =  process.env.PORT || 3000;
 
 app.use(bodyParser.json());
